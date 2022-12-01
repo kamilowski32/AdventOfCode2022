02f692ee0fda95c569b066a3415c5e75f47934c8
kamilowski32 <74541018+kamilowski32@users.noreply.github.com>
2022-12-01 14:28:36 +0100

    Day 1

---
+++ Day1.1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+int main()
+{
+    long currElf = 0, max = 0;
+    std::string currCalories;
+    while (getline(std::cin, currCalories))
+    {
+        if (currCalories == "x")
+            break;
+        currCalories == "" ? (currElf > max ? max = currElf, currElf = 0 : currElf = 0) : (currElf += std::stol(currCalories));
+    }
+    std::cout << max;
+}
---
+++ Day1.2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+int main()
+{
+    long currElf = 0, top1 = 0, top2 = 0, top3 = 0, temp;
+    std::string currCalories;
+    while (getline(std::cin, currCalories))
+    {
+        if (currCalories == "x")
+            break;
+        else if (currCalories == "")
+            currElf > top3 ? (currElf > top2 ? (currElf > top1 ? (top3 = top2, top2 = top1, top1 = currElf, currElf = 0) : (top3 = top2, top2 = currElf, currElf = 0)) : (top3 = currElf, currElf = 0)) : (currElf = 0);
+        else
+            currElf += std::stol(currCalories);
+    }
+    std::cout << top1 + top2 + top3;
+}
