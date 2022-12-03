abcebe58f93ea1c80223ce52fafedea2b077c84e
kamilowski32 <74541018+kamilowski32@users.noreply.github.com>
2022-12-03 07:05:08 +0100

    Day 3

---
+++ Day3.1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+char sameLetter;
+
+int valueOfChar()
+{
+	if (sameLetter >= 'A' && sameLetter <= 'Z')
+		return (int)sameLetter - 38;
+	return (int)sameLetter - 96;
+}
+
+
+int main() {
+	string input;
+	int sum = 0;
+	while (getline(cin, input))
+	{
+		if (input == "x")
+			break;
+		int mid = (input.length() + 1) / 2;
+		string firstPart = input.substr(0, mid);
+		string secondPart = input.substr(mid);
+		for (auto i : firstPart)
+		{
+			if (secondPart.find(i) != string::npos)
+			{
+				sameLetter = i;
+				break;
+			}
+		}
+		sum += valueOfChar();
+	}
+	cout << sum;
+}
---
+++ Day3.2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+int valueOfChar(char sameLetter)
+{
+	if (sameLetter >= 'A' && sameLetter <= 'Z')
+		return (int)sameLetter - 38;
+	return (int)sameLetter - 96;
+}
+
+
+void remove(vector<char>& vect, const char& elem)
+{
+	vect.erase(remove(vect.begin(), vect.end(), elem), vect.end());
+}
+
+int main() {
+	vector<char> commonLetters;
+	string input;
+	int sum = 0;
+	while (getline(cin, input)) //first line
+	{
+		if (input == "x")
+			break;
+		string secondInput;
+		getline(cin, secondInput); //secondline
+		for (auto i : input)
+		{
+			if (secondInput.find(i) != string::npos)
+			{
+				commonLetters.push_back(i);
+			}
+		}
+		getline(cin, secondInput); //thirdLine
+		for (auto i : input)
+		{
+			if (secondInput.find(i) == string::npos)
+			{
+				remove(commonLetters, i);
+			}
+		}
+		sum += valueOfChar(commonLetters[0]);
+		commonLetters.clear();
+	}
+	cout << sum;
+}
