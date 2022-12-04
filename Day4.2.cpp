9f582339a5fcbc00d86fc04df4f395c10a19ac5d
kamilowski32 <74541018+kamilowski32@users.noreply.github.com>
2022-12-04 12:30:48 +0100

    Day 4

---
+++ Day4.2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+	int howMany = 0;
+	while (true)
+	{
+		string input;
+		cin >> input;
+		if (input == "x")
+			break;
+		int begin1, begin2, end1, end2;
+		begin1 = stoi(input.substr(0, input.find('-')));
+		input.erase(0, input.find('-') + 1);
+		end1 = stoi(input.substr(0, input.find(',')));
+		input.erase(0, input.find(',') + 1);
+		begin2 = stoi(input.substr(0, input.find('-')));
+		input.erase(0, input.find('-') + 1);
+		end2 = stoi(input);
+		if (begin1 < begin2)
+		{
+			if (end1 >= begin2)	howMany++;
+		}
+		else if (begin2 < begin1)
+		{
+			if (end2 >= begin1)	howMany++;
+		}
+		else
+		{
+			howMany++;
+		}
+	}
+	cout << howMany;
+}
