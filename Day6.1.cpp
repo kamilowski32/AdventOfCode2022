767b617c624d5825cab20d628f90bcae3e9d00c0
kamilowski32 <74541018+kamilowski32@users.noreply.github.com>
2022-12-06 09:27:31 +0100

    Day 6

---
+++ Day6.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool anySame(char c1, char c2, char c3, char c4)
+{
+    return c1 == c2 || c1 == c3 || c1 == c4 || c2 == c3 || c2 == c4 || c3 == c4;
+}
+
+int main()
+{
+    string input;
+    cin >> input;
+    char c1 = input.at(0), c2 = input.at(1), c3 = input.at(2), c4 = input.at(3);
+    int i = 4;
+    while (anySame(c1, c2, c3, c4))
+    {
+        i++;
+        c1 = c2;
+        c2 = c3;
+        c3 = c4;
+        c4 = input.at(i - 1);
+    }
+    cout << i << endl;
+}
---
+++ Day6.2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool anySame(vector<short> vect)
+{
+    for (auto i : vect)
+    {
+        if (i > 1)
+            return true;
+    }
+    return false;
+}
+
+int main()
+{
+    string input;
+    cin >> input;
+    vector<short> vect;
+    vect.resize(26);
+    for (int iter = 0; iter < 14; iter++)
+    {
+        vect[(int)input.at(iter) - 97]++;
+    }
+    int i = 14;
+    while (anySame(vect))
+    {
+        i++;
+        vect[input.at(i - 15) - 97]--;
+        vect[input.at(i - 1) - 97]++;
+    }
+    cout << i << endl;
+}
