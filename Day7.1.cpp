62fbeb159cffb3dbb3d8ef2ec1e41ccbdc2828dc
kamilowski32 <74541018+kamilowski32@users.noreply.github.com>
2022-12-07 14:59:25 +0100

    Day 7

---
+++ Day7.1.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string input;
+	vector<int> sizes;
+	int sum = 0;
+	ifstream plik;
+	plik.open("Day7.txt");
+	while (getline(plik, input))
+	{
+		if (input[0] == '$')
+		{
+			if (input[2] == 'c')
+			{
+				if (input[5] != '.')
+				{
+					sizes.push_back(0);
+				}
+				else
+				{
+					int temp = sizes[sizes.size() - 1];
+					sizes.pop_back();
+					if (temp <= 100000)
+						sum += temp;
+					sizes[sizes.size() - 1] += temp;
+				}
+			}
+		}
+		else if (input[0] == 'd')
+		{
+
+		}
+		else if (input[0] >= '0' && input[0] <= '9')
+		{
+			string size = input.substr(0, input.find(' ') + 4);
+			sizes[sizes.size() - 1] += stoi(size);
+		}
+	}
+	plik.close();
+	while(sizes.size()>1)
+	{
+		int temp = sizes[sizes.size() - 1];
+		sizes.pop_back();
+		if (temp <= 100000)
+			sum += temp;
+		sizes[sizes.size() - 1] += temp;
+	}
+	sizes.clear();
+	cout << sum;
+}
---
+++ Day7.2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string input;
+	vector<int> sizes, sizes2;
+	int sum = 0, free = 70000000, currMin = 30000000;
+	ifstream plik;
+	plik.open("Day7.txt");
+	while (getline(plik, input))
+	{
+		if (input[0] == '$')
+		{
+			if (input[2] == 'c')
+			{
+				if (input[5] != '.')
+				{
+					sizes.push_back(0);
+				}
+				else
+				{
+					int temp = sizes[sizes.size() - 1];
+					sizes.pop_back();
+					sizes[sizes.size() - 1] += temp;
+					sizes2.push_back(temp);
+				}
+			}
+		}
+		else if (input[0] >= '0' && input[0] <= '9')
+		{
+			string size = input.substr(0, input.find(' ') + 4);
+			sizes[sizes.size() - 1] += stoi(size);
+		}
+	}
+	plik.close();
+	while (sizes.size() > 1)
+	{
+		int temp = sizes[sizes.size() - 1];
+		sizes.pop_back();
+		sizes[sizes.size() - 1] += temp;
+		sizes2.push_back(temp);
+	}
+	free -= sizes[0];
+	sizes.clear();
+	for (auto i : sizes2)
+	{
+		if (i + free > 30000000 && i < currMin)
+		{
+			currMin = i;
+		}
+	}
+	cout << currMin;
+}
