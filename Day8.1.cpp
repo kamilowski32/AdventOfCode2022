451b3fdaa0cab8978979a6e92e40fb1c7b68b8e6
kamilowski32 <74541018+kamilowski32@users.noreply.github.com>
2022-12-08 07:50:12 +0100

    Day 8

---
+++ Day8.1.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	fstream file;
+	file.open("Day8.txt");
+	vector<string> trees;
+	string temp;
+	int sum = 0;
+	while (getline(file, temp))
+	{
+		trees.push_back(temp);
+	}
+	file.close();
+	int size = trees.size();
+	bool** isItVisible = new bool* [size];
+	for (int i = 0; i < size; i++)
+	{
+		isItVisible[i] = new bool[size];
+		for (int j = 0; j < size; j++)
+			isItVisible[i][j] = false;
+	}
+
+
+	int currMax = -1;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			int temp = trees[i][j] - '0';
+			if (temp > currMax)
+			{
+				currMax = temp;
+				isItVisible[i][j] = true;
+			}
+		}
+		currMax = -1;
+		for (int j = size - 1; j >= 0; j--)
+		{
+			int temp = trees[i][j] - '0';
+			if (temp > currMax)
+			{
+				currMax = temp;
+				isItVisible[i][j] = true;
+			}
+		}
+		currMax = -1;
+	}
+	currMax = -1;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			int temp = trees[j][i] - '0';
+			if (temp > currMax)
+			{
+				currMax = temp;
+				isItVisible[j][i] = true;
+			}
+		}
+		currMax = -1;
+		for (int j = size - 1; j >= 0; j--)
+		{
+			int temp = trees[j][i] - '0';
+			if (temp > currMax)
+			{
+				currMax = temp;
+				isItVisible[j][i] = true;
+			}
+		}
+		currMax = -1;
+	}
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+			if (isItVisible[j][i])
+				sum++;
+	}
+	for (int i = 0; i < size; i++)
+	{
+		delete[] isItVisible[i];
+	}
+	delete[] isItVisible;
+	cout << sum << endl;
+}
---
+++ Day8.2.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	fstream file;
+	file.open("Day8.txt");
+	vector<string> trees;
+	string temp;
+	while (getline(file, temp))
+	{
+		trees.push_back(temp);
+	}
+	file.close();
+	int size = trees.size();
+
+    int maximum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            int currTree = trees[i][j] -'0';
+            int up = 0, down = 0, left = 0, right = 0;
+            for (int u = i - 1; u >= 0; u--) // up
+            {
+                up++;
+                if (trees[u][j] - '0' >= currTree) break;
+            }
+            for (int d = i + 1; d < size; d++) // down
+            {
+                down++;
+                if (trees[d][j] - '0' >= currTree) break;
+            }
+            for (int l = j - 1; l >= 0; l--) // left
+            {
+                left++;
+                if (trees[i][l] - '0' >= currTree) break;
+            }
+            for (int r = j + 1; r < size; r++) // right
+            {
+                right++;
+                if (trees[i][r] - '0' >= currTree) break;
+            }
+            int currScore = up * down * left * right;
+            if (currScore > maximum)
+            {
+                maximum = currScore;
+            }
+        }
+    }
+	cout << maximum << endl;
+}
